a312a5d6c329d258269959d45d8bf52647307ec4
akshin-autods <agshin@autods.com>
2024-11-15 23:22:25 +0100

    update config , add DB_URL

--- src/config.py
+++ src/config.py
@@ -4,10 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     BOT_TOKEN: str = ""
     ADMIN_IDS: list[int] = []
+    DB_URL: str = "sqlite://db.sqlite3"
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
+
 
 settings = Settings()
