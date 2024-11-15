8e3599433d8c29d2a98df1e891ada3e52d3d843e
akshin-autods <agshin@autods.com>
2024-11-15 23:22:11 +0100

    delete client handler

--- src/handlers/client.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router
-from aiogram.types import Message
-
-router = Router()
-
-
-@router.message()
-async def activate_handler(message: Message) -> None:
-    await message.answer("Activated! client")
